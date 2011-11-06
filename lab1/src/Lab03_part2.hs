3f3383831b7d5a48940f61069cfc26f2e057d52c
fembem <leo.decandia@gmail.com>
2011-11-05 15:26:24 -1000

    implemented all functions for quaternion

--- lab1/src/Lab03_part2.hs
+++ lab1/src/Lab03_part2.hs
@@ -41,25 +41,43 @@ instance Show Quaternion where
 
 --}
 
+{--
+
+let [a1, b1, c1, d1, a2, b2, c2, d2] = [1.0:: Double, 1.0:: Double,1.0:: Double,1.0:: Double, 1.0:: Double,1.0:: Double,1.0:: Double,1.0:: Double]
+
+(Quaternion a1 b1 c1 d1) * (Quaternion a2 b2 c2 d2) =
+    (Quaternion (a1 * a2 − b1 * b2 − c1 * c2 − d1 * d2)
+               (a1 * b2 + b1 * a2 + c1 * d2 − d1 * c2)
+               (a1 * c2 − b1 * d2 + c1 * a2 + d1 * b2)
+               (a1 * d2 + b1 * c2 − c1 * b2 + d1 * a2) )
+
+fromInteger i = (Quaternion i 0 0 0)
+
+signum (Quaternion a b c d) =
+    let val = abs (Quaternion a b c d)
+    in Quaternion (a / val) (b / val) (c / val) (d / val)
+
+abs (Quaternion a b c d) = (Quaternion (Math.sqrt ( (a * a) ( b * b) (c * c) (d * d) ) 0.0 0.0 0.0 )
+--}
+
+
 instance Num Quaternion where
+
+    abs (Quaternion a b c d) = Quaternion ( sqrt (a * a + b * b + c * c + d * d)) 0.0 0.0 0.0
 
     (Quaternion a1 b1 c1 d1) + (Quaternion a2 b2 c2 d2) = Quaternion (a1 + a2) (b1 + b2) (c1 + c2) (d1 + d2)
 
     negate (Quaternion a b c d) = Quaternion (-a) (-b) (-c) (-d)
 
-    abs (Quaternion a b c d) = (Quaternion (Math.sqrt ( (a * a) ( b * b) (c * c) (d * d) ) 0.0 0.0 0.0 )
-
-    {-
     (Quaternion a1 b1 c1 d1) * (Quaternion a2 b2 c2 d2) =
-        (Quaternion (a1 * a2 − b1 * b2 − c1 * c2 − d1 * d2)
-                   (a1 * b2 + b1 * a2 + c1 * d2 − d1 * c2)
-                   (a1 * c2 − b1 * d2 + c1 * a2 + d1 * b2)
-                   (a1 * d2 + b1 * c2 − c1 * b2 + d1 * a2) )
-    -}
-    fromInteger i = (Quaternion i 0 0 0)
+      (Quaternion (a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2)
+      (a1 * b2 + b1 * a2 + c1 * d2 - d1 * c2)
+      (a1 * c2 - b1 * d2 + c1 * a2 + d1 * b2)
+      (a1 * d2 + b1 * c2 - c1 * b2 + d1 * a2) )
+
+    fromInteger i = (Quaternion (fromIntegral i ) 0.0 0.0 0.0)
 
     signum (Quaternion a b c d) =
-        let val = abs (Quaternion a b c d)
-        in Quaternion (a / val) (b / val) (c / val) (d / val)
-
+        let (Quaternion val _ _ _) = abs (Quaternion a b c d)
+            in Quaternion (a / val) (b / val) (c / val) (d / val)
 
