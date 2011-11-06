e698310c8c34502fe635d2894ed26e9973a8b41c
fembem <leo.decandia@gmail.com>
2011-11-05 14:52:09 -1000

    implemented a few Num functions for quaternion

---
+++ lab1/src/Lab03_part2.hs
@@ -0,0 +1,65 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  Lab03_part2
+-- Copyright   :
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Lab03_part2 (
+
+) where
+
+data Quaternion = Quaternion Double Double Double Double deriving (Eq)
+
+{-- "1.0 + 2.0i + 3.0j + 4.0k" --}
+showQuaternion:: Quaternion -> String
+showQuaternion (Quaternion a b c d) = ( (show a) ++ " + " ++ (show b) ++ "i"
+                                      ++ " + " ++ (show c) ++ "j"
+                                      ++ " + " ++ (show d) ++ "k")
+
+instance Show Quaternion where
+    show =  showQuaternion
+
+{--
+
+    i * i = -1
+    j * j = -1
+    k * k = -1
+    i * j =  k
+    j * i = -k
+    j * k =  i
+    k * j = -i
+    k * i =  j
+    i * k = -j
+
+--}
+
+instance Num Quaternion where
+
+    (Quaternion a1 b1 c1 d1) + (Quaternion a2 b2 c2 d2) = Quaternion (a1 + a2) (b1 + b2) (c1 + c2) (d1 + d2)
+
+    negate (Quaternion a b c d) = Quaternion (-a) (-b) (-c) (-d)
+
+    abs (Quaternion a b c d) = (Quaternion (Math.sqrt ( (a * a) ( b * b) (c * c) (d * d) ) 0.0 0.0 0.0 )
+
+    {-
+    (Quaternion a1 b1 c1 d1) * (Quaternion a2 b2 c2 d2) =
+        (Quaternion (a1 * a2 − b1 * b2 − c1 * c2 − d1 * d2)
+                   (a1 * b2 + b1 * a2 + c1 * d2 − d1 * c2)
+                   (a1 * c2 − b1 * d2 + c1 * a2 + d1 * b2)
+                   (a1 * d2 + b1 * c2 − c1 * b2 + d1 * a2) )
+    -}
+    fromInteger i = (Quaternion i 0 0 0)
+
+    signum (Quaternion a b c d) =
+        let val = abs (Quaternion a b c d)
+        in Quaternion (a / val) (b / val) (c / val) (d / val)
+
+
