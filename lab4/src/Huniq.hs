c2283c7b9694c1537268ac66a913c1355358d655
fembem <leo.decandia@gmail.com>
2011-11-10 09:19:00 -1000

    all programs now working

--- lab4/src/Huniq.hs
+++ lab4/src/Huniq.hs
@@ -18,7 +18,6 @@ module Huniq (
 
 import System.Environment
 import System.IO
-import Data.List
 import Control.Monad
 
 main = do
@@ -27,44 +26,28 @@ main = do
     handle <- openFile fileName ReadMode
     contents <- hGetContents handle
     let fileLines = lines contents
-    let filteredLines = foldl1 helperFunc ([] , () ) fileLines
+    let (filteredLines, last) = foldr (\x acc ->
+          let (list, last) = acc in if x == last then (list, x) else (x:list, x))
+              ([] , "" ) fileLines
+    {-let (filteredLines, _) = foldr helperFunc ([] , "" ) fileLines-}
     putStr $ unlines filteredLines
     hClose handle
     return ()
     )
   return ()
 
-helperFunc :: ([String], String) -> String -> ([String], String)
-helperFunc accum x =
+helperFunc ::  String -> ([String], String)   -> ([String], String)
+helperFunc x accum  =
   let (list, last) = accum
-  in if x == last then (list, x) else (x : list, x)
+  in if x == last then (list, x) else (x:list, x)
 
-
-{- code from http://learnyouahaskell.com/recursion -}
-quicksort :: (Ord a) => [a] -> [a]
-quicksort [] = []
-quicksort (x:xs) =
-  let smallerSorted = quicksort [a | a <- xs, a <= x]
-      biggerSorted = quicksort [a | a <- xs, a > x]
-  in  smallerSorted ++ [x] ++ biggerSorted
-
-{-
-
-main = do
-  args <- getArgs
-  forM_ args (\fileName -> do
-    handle <- openFile fileName ReadMode
-    contents <- hGetContents handle
-    let fileLines = lines contents
-    let filteredLines = foldl1 (\ accum x ->
-                  let (list, last) = accum in
-                    if x == last then (list, x) else (x : list, x) )
-                  ([] , () )
-                  fileLines
-    putStr $ unlines filteredLines
-    hClose handle
-    return ()
-    )
-  return ()
-
--}
+{-testing-}
+{-a-}
+{-a-}
+{-b-}
+{-b-}
+{-b-}
+{-c-}
+{-b-}
+{-c-}
+{-c-}
